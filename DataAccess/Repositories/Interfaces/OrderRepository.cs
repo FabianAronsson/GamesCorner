e082a445589cd4d32d5c19d0146bfd8c8ab58455
alexgoold <102036021+alexgoold@users.noreply.github.com>
2023-04-04 11:57:06 +0200

    Created Concrete Order Repo

---
+++ DataAccess/Repositories/Interfaces/OrderRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Models;
+
+namespace DataAccess.Repositories.Interfaces
+{
+	public class OrderRepository : IOrderRepository
+	{
+		public async Task<OrderModel> GetAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<IEnumerable<OrderModel>> GetAllAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<OrderModel> AddAsync(OrderModel entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<OrderModel> UpdateAsync(OrderModel entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<OrderModel> DeleteAsync(OrderModel entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
