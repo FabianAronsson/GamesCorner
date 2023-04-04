ecdf53e576112dbc7d958a2b5b2f1b064ef6c2be
coldmagpie <coldmagpie@gmail.com>
2023-04-04 14:44:34 +0200

    create get product request

---
+++ GamesCorner/Server/Requests/GetProductRequest.cs
@@ -0,0 +1,10 @@
+﻿using GamesCorner.Server.Requests.Interface;
+
+namespace GamesCorner.Server.Requests
+{
+    public class GetProductRequest : IHttpRequest
+    {
+        public Guid Id { get; set; }
+        public HttpRequest HttpRequest { get; set; }
+    }
+}
