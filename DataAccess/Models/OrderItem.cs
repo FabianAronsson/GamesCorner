b57c333c6d2049075b717c2e4e91364d0743f7ae
coldmagpie <coldmagpie@gmail.com>
2023-04-04 11:36:25 +0200

    create orderitem model

--- DataAccess/Models/OrderItem.cs
+++ DataAccess/Models/OrderItem.cs
@@ -8,5 +8,9 @@ namespace DataAccess.Models
 {
 	public class OrderItem
 	{
+		public Guid Id { get; set; }
+		public Guid ProductId { get; set; }
+		public int Amount { get; set; }
+        public double Price { get; set; }
 	}
 }
