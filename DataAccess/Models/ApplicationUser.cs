b171ed325dba449d82685c51c71f7136bae6c5a1
coldmagpie <coldmagpie@gmail.com>
2023-04-04 13:47:00 +0200

    update user model

--- DataAccess/Models/ApplicationUser.cs
+++ DataAccess/Models/ApplicationUser.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 
 namespace GamesCorner.Server.Models
 {
 	public class ApplicationUser : IdentityUser
 	{
-		public string Adress { get; set; }
-		public string PostNumber { get; set; }
+		[Required]
+		public string Address { get; set; }
+		[MaxLength(4)]
+        public string  PostCode{ get; set; }
+		[Required]
 		public string City { get; set; }
-	}
+    }
 }
