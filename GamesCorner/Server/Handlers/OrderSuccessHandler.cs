504868afdeeb3e5d44357a38e53dba36f1d3d92d
alexgoold <102036021+alexgoold@users.noreply.github.com>
2023-04-05 12:02:56 +0200

    Added OrderSuccess Endpoint

---
+++ GamesCorner/Server/Handlers/OrderSuccessHandler.cs
@@ -0,0 +1,26 @@
+﻿using Duende.IdentityServer.Models;
+using GamesCorner.Server.Requests;
+using MediatR;
+using Stripe.Checkout;
+
+namespace GamesCorner.Server.Handlers
+{
+	public class OrderSuccessHandler: IRequestHandler<OrderSuccessRequest, IResult>
+	{
+		public async Task<IResult> Handle(OrderSuccessRequest request, CancellationToken cancellationToken)
+		{
+			
+			var sessionService = new SessionService();
+
+			var session = await sessionService.GetAsync(request.Session_Id);
+
+			var customerEmail = session.CustomerDetails.Email;
+
+			await request.UnitOfWork.OrderRepository.AddAsync(request.OrderObject);
+
+			return Results.Ok();
+
+
+		}
+	}
+}
--- GamesCorner/Server/Requests/OrderSuccessRequest.cs
+++ GamesCorner/Server/Requests/OrderSuccessRequest.cs
@@ -1,6 +1,16 @@
-﻿namespace GamesCorner.Server.Requests
+﻿using DataAccess.Models;
+using DataAccess.UnitOfWork;
+using GamesCorner.Server.Requests.Interface;
+
+namespace GamesCorner.Server.Requests
 {
-	public class OrderSuccessRequest
+	public class OrderSuccessRequest :IHttpRequest
 	{
+		public string Session_Id { get; set; }
+
+		public OrderModel OrderObject { get; set; }
+
+		public UnitOfWork UnitOfWork { get; set; }
+
 	}
 }
