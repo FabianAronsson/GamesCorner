8338ff04bb437710ebc6d682b11070d5a2850568
coldmagpie <coldmagpie@gmail.com>
2023-04-05 14:32:19 +0200

    AddTiCartHandler

--- GamesCorner/Server/Requests/AddToCartRequest.cs
+++ GamesCorner/Server/Requests/AddToCartRequest.cs
@@ -1,6 +1,13 @@
-﻿namespace GamesCorner.Server.Requests
+﻿using DataAccess.Models;
+using DataAccess.UnitOfWork;
+using GamesCorner.Server.Requests.Interface;
+
+namespace GamesCorner.Server.Requests
 {
-    public class AddToCartRequest
+    public class AddToCartRequest : IHttpRequest
     {
+        public OrderItem item { get; set; }
+        public HttpContext HttpContext { get; set; }
+        public IUnitOfWork UnitOfWork { get; set; }
     }
 }
