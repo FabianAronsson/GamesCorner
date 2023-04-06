ad1a064d7fd2488dc3e8d83400b6ba07c4e64fa1
FabianAronsson <fabianaronsson2@gmail.com>
2023-04-06 10:21:02 +0200

    Create sessionId handler

--- GamesCorner/Server/Requests/CreateSessionIdRequest.cs
+++ GamesCorner/Server/Requests/CreateSessionIdRequest.cs
@@ -1,9 +1,10 @@
 ﻿using DataAccess.Models;
+using GamesCorner.Server.Requests.Interface;
 using GamesCorner.Server.Services.PaymentService;
 
 namespace GamesCorner.Server.Requests;
 
-public class CreateSessionIdRequest
+public class CreateSessionIdRequest : IHttpRequest
 {
     public List<OrderItem> Cart { get; set; }
     public IPaymentService PaymentService { get; set; }
