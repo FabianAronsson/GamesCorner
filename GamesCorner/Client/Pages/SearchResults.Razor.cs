4acfd77c5ba76e7020c90cccd42fe30f97181880
alexgoold <102036021+alexgoold@users.noreply.github.com>
2023-04-06 15:23:48 +0200

    Add search page Add ProductModelDto

---
+++ GamesCorner/Client/Pages/SearchResults.Razor.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http.Json;
+using GamesCorner.Shared.DTOs;
+using Microsoft.AspNetCore.Components;
+
+namespace GamesCorner.Client.Pages
+{
+	partial class SearchResults : ComponentBase
+	{
+		[Parameter] public string SearchTerm { get; set; } = "";
+
+		private List<ProductModelDto>? _products = new();
+
+		protected override async Task OnParametersSetAsync()
+		{
+			await Search();
+			await base.OnParametersSetAsync();
+		}
+
+		private async Task Search()
+		{
+			var client = HttpClientFactory.CreateClient("public");
+
+			var response = await client.GetAsync($"search?name={SearchTerm}");
+
+			if (response.IsSuccessStatusCode)
+			{
+				_products = await response.Content.ReadFromJsonAsync<List<ProductModelDto>>();
+			}
+			StateHasChanged();
+
+			
+		}
+	}
+}
