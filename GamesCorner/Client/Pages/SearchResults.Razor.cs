985e8d266ad4057fbac6ab7ca3bccbf119a4a182
alexgoold <102036021+alexgoold@users.noreply.github.com>
2023-04-11 12:04:55 +0200

    Added filter combobox added filter functionality

--- GamesCorner/Client/Pages/SearchResults.Razor.cs
+++ GamesCorner/Client/Pages/SearchResults.Razor.cs
@@ -9,6 +9,10 @@ namespace GamesCorner.Client.Pages
 		[Parameter] public string SearchTerm { get; set; } = "";
 
 		private List<ProductModelDto>? _products = new();
+		private List<ProductModelDto>? _filteredProducts = new();
+		public List<string> categories = new();
+
+
 
 		protected override async Task OnParametersSetAsync()
 		{
@@ -18,17 +22,50 @@ namespace GamesCorner.Client.Pages
 
 		private async Task Search()
 		{
+
 			var client = HttpClientFactory.CreateClient("public");
 
 			var response = await client.GetAsync($"search?name={SearchTerm}");
 
 			if (response.IsSuccessStatusCode)
 			{
-				_products = await response.Content.ReadFromJsonAsync<List<ProductModelDto>>();
+				_filteredProducts = await response.Content.ReadFromJsonAsync<List<ProductModelDto>>();
 			}
 			StateHasChanged();
 
-			
+		}
+
+		private void FilterResultsByCategory(ChangeEventArgs obj)
+		{
+
+			var category = obj.Value;
+			if (category is not null)
+			{
+				_filteredProducts = _products.Where(x => x.Category == category.ToString()).ToList();
+			}
+
+			StateHasChanged();
+		}
+
+		protected override async Task OnInitializedAsync()
+		{
+			await GetAllProducts();
+			await base.OnInitializedAsync();
+		}
+
+		private async Task GetAllProducts()
+		{
+			var client = HttpClientFactory.CreateClient("public");
+
+			var response = await client.GetAsync($"search?name={""}");
+
+			if (response.IsSuccessStatusCode)
+			{
+				_products = await response.Content.ReadFromJsonAsync<List<ProductModelDto>>();
+			}
+
+			categories = _products.Select(x => x.Category).Distinct().ToList();
+			StateHasChanged();
 		}
 	}
 }
