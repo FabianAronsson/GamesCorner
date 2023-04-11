176153bd877d687a006ba4961c9af84e56f79731
alexgoold <102036021+alexgoold@users.noreply.github.com>
2023-04-11 19:21:09 +0200

    Add banner image

--- GamesCorner/Shared/DTOs/ProductModelDto.cs
+++ GamesCorner/Shared/DTOs/ProductModelDto.cs
@@ -11,6 +11,8 @@ namespace GamesCorner.Shared.DTOs
 		public string Id { get; set; }
 		public string Name { get; set; }
 		public string ImageUrl { get; set; }
+
+		public string BannerUrl { get; set; }
 		public double Price { get; set; }
 		public string Category { get; set; }
 		public string Description { get; set; }
