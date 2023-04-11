1ca2048dbdbd5ba381dae6ee17c483cba9b91c65
alexgoold <102036021+alexgoold@users.noreply.github.com>
2023-04-11 15:08:50 +0200

    Start product info page

---
+++ GamesCorner/Client/Pages/ProductInfo.razor.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http.Json;
+using GamesCorner.Shared.DTOs;
+using Microsoft.AspNetCore.Components;
+
+namespace GamesCorner.Client.Pages
+{
+	partial class ProductInfo : ComponentBase
+	{
+		[Parameter] public string ProductId { get; set ; }
+
+		public ProductModelDto Product { get; set; } = new();
+
+		protected override async Task OnParametersSetAsync()
+		{
+			await GetProduct();
+			await base.OnParametersSetAsync();
+		}
+
+		private async Task GetProduct()
+		{
+			var GuidFromString = new Guid(ProductId);
+			var client = HttpClientFactory.CreateClient("public");
+
+			var response = await client.GetAsync($"getProduct?id={GuidFromString}");
+
+			if (response.IsSuccessStatusCode)
+			{
+				Product = await response.Content.ReadFromJsonAsync<ProductModelDto>();
+			}
+			StateHasChanged();
+		}
+	}
+}
