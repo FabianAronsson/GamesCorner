53be509b0d33ac4bce1d3d7dcb080c2e568d8d68
alexgoold <102036021+alexgoold@users.noreply.github.com>
2023-04-12 13:23:09 +0200

    Fix review for mobile screens remove a line in productModelDto

--- GamesCorner/Shared/DTOs/ProductModelDto.cs
+++ GamesCorner/Shared/DTOs/ProductModelDto.cs
@@ -11,7 +11,6 @@ namespace GamesCorner.Shared.DTOs
 		public string Id { get; set; }
 		public string Name { get; set; }
 		public string ImageUrl { get; set; }
-
 		public string BannerUrl { get; set; }
 		public double Price { get; set; }
 		public string Category { get; set; }
