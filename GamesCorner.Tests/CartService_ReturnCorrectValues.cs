2b973f3df08ccdf69bd35272f5f081020d72a8c9
FabianAronsson <fabianaronsson2@gmail.com>
2023-04-13 08:15:23 +0200

    Add small test for cart service

---
+++ GamesCorner.Tests/CartService_ReturnCorrectValues.cs
@@ -0,0 +1,27 @@
+﻿using Blazored.LocalStorage;
+using FakeItEasy;
+using GamesCorner.Client.Services.CartService;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace GamesCorner.Tests;
+
+public class CartService_ReturnCorrectValues
+{
+    [Fact]
+    public async Task GetUserId_ReturnStringOrNull()
+    {
+        //Arrange
+        var localStorage = A.Fake<ILocalStorageService>();
+        var httpClientFactory = A.Fake<IHttpClientFactory>();
+        var httpClient = A.Fake<HttpClient>();
+        var authState = A.Fake<AuthenticationStateProvider>();
+
+        var sut = new CartService(localStorage, httpClientFactory, httpClient, authState);
+
+        //Act
+        var result = await sut.GetUserId();
+
+        //Assert
+        Assert.True(result is { } or string );
+    }
+}
