d3fe7a24b17bba2914e8b7326c549086ce838410
alexgoold <102036021+alexgoold@users.noreply.github.com>
2023-04-17 22:18:47 +0200

    Update Resources.resx so games have multiple categories Create filtering logic and ui to filter and order Product search results

--- GamesCorner/Client/Pages/SearchResults.Razor.cs
+++ GamesCorner/Client/Pages/SearchResults.Razor.cs
@@ -11,47 +11,90 @@ namespace GamesCorner.Client.Pages
 		private List<ProductModelDto>? _products = new();
 		private List<ProductModelDto>? _filteredProducts = new();
 		public List<string> categories = new();
+		public List<int> ages = new();
+		public List<double> prices = new() { 100, 200, 300, 400, 500, 600, 700, 800, 900 };
+		public int selectedAge = 100;
+		public double selectedPrice = int.MaxValue;
+		public string selectedCategory { get; set; } = "";
 
-      
-
-        protected override async Task OnParametersSetAsync()
+		protected override async Task OnParametersSetAsync()
 		{
             await GetAllProducts();
         }
 
-        private async Task Search()
+
+		private void FilterResultsByAge(ChangeEventArgs obj)
 		{
+			var filter = obj.Value;
+			var checkType = obj.GetType();
 
-			var client = HttpClientFactory.CreateClient("public");
+			int.TryParse(filter.ToString(), out selectedAge);
 
-			var response = await client.GetAsync($"search?name={SearchTerm}");
 
-			if (response.IsSuccessStatusCode)
+			if (filter.ToString() == "Age...")
 			{
-				_filteredProducts = await response.Content.ReadFromJsonAsync<List<ProductModelDto>>();
+				selectedAge = 100;
 			}
-			StateHasChanged();
 
+			_filteredProducts = _products.Where(p => p.Category.Contains(selectedCategory) && p.Price <= selectedPrice && p.AgeRestriction <= selectedAge).ToList();
+
+			StateHasChanged();
+		}
+
+		private void FilterResultsByPrice(ChangeEventArgs obj)
+		{
+			var filter = obj.Value;
+
+			double.TryParse(filter.ToString(), out selectedPrice);
+
+			if (filter.ToString() == "Price...")
+			{
+				selectedPrice = int.MaxValue;
+			}
+			_filteredProducts = _products.Where(p => p.Category.Contains(selectedCategory) && p.Price <= selectedPrice && p.AgeRestriction <= selectedAge).ToList();
+
+
+			StateHasChanged();
 		}
 
 		private void FilterResultsByCategory(ChangeEventArgs obj)
 		{
-			var category = obj.Value;
+			var filter = obj.Value;
 
-			if (category.ToString() == "All")
+			selectedCategory = filter.ToString();
+
+			if (selectedCategory == "Category...")
 			{
-				_filteredProducts = _products;
+				selectedCategory = "";
 			}
-			else
-			{
-				_filteredProducts = _products.Where(x => x.Category == category.ToString()).ToList();
-			
-			}
+
+			_filteredProducts = _products.Where(p => p.Category.Contains(selectedCategory) && p.Price <= selectedPrice && p.AgeRestriction <= selectedAge).ToList();
+
 
 			StateHasChanged();
 		}
 
+		private void OrderBy(ChangeEventArgs obj)
+		{
+			var value = obj.Value;
 
+			int.TryParse(value.ToString(), out int switchValue);
+
+			switch (switchValue)
+			{
+				case 1:
+					_filteredProducts = _products.Where(p => p.Category.Contains(selectedCategory) && p.Price <= selectedPrice && p.AgeRestriction <= selectedAge).OrderBy(x => x.Price).ToList();
+					break;
+				case 2:
+					_filteredProducts = _products.Where(p => p.Category.Contains(selectedCategory) && p.Price <= selectedPrice && p.AgeRestriction <= selectedAge).OrderByDescending(x => x.Price).ToList();
+					break;
+				case 3:
+					_filteredProducts = _products.Where(p => p.Category.Contains(selectedCategory) && p.Price <= selectedPrice && p.AgeRestriction <= selectedAge).OrderBy(x => x.Name).ToList();
+					break;
+			}
+			StateHasChanged();
+		}
+		
 		private async Task GetAllProducts()
 		{
 			var client = HttpClientFactory.CreateClient("public");
@@ -63,7 +106,9 @@ namespace GamesCorner.Client.Pages
 				_products = await response.Content.ReadFromJsonAsync<List<ProductModelDto>>();
 			}
 
-			categories = _products.Select(x => x.Category).Distinct().ToList();
+			categories = _products.Select(x => x.Category.Split(" ").First()).Distinct().ToList();
+			ages = _products.Select(x => x.AgeRestriction).Distinct().ToList();
+			_filteredProducts = _products;
 			StateHasChanged();
 		}
 	}
