1d073253b5352d46dd3c08bf61497f429d0a159f
FabianAronsson <fabianaronsson2@gmail.com>
2023-04-17 16:49:04 +0200

    Add user component

---
+++ GamesCorner/Client/Pages/Enums/ComponentType.cs
@@ -0,0 +1,7 @@
+﻿namespace GamesCorner.Client.Pages.Enums;
+
+public enum ComponentType
+{
+    Profile,
+    Orders
+}
---
+++ GamesCorner/Shared/Dtos/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace GamesCorner.Shared.Dtos;
+
+public class UserDto
+{
+    public string Id { get; set; }
+    public string? UserName { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? PostalCode { get; set; }
+    public string? City { get; set; }
+    public string? Address { get; set; }
+}
