bd107c53972c8381ac4726c125cfdae1f654f51e
coldmagpie <coldmagpie@gmail.com>
2023-04-19 12:04:36 +0200

    create create product handler

---
+++ GamesCorner/Server/Handlers/CreateProductHandler.cs
@@ -0,0 +1,21 @@
+﻿using GamesCorner.Server.Requests;
+using MediatR;
+
+namespace GamesCorner.Server.Handlers
+{
+    public class CreateProductHandler : IRequestHandler<CreateProductRequest, IResult>
+    {
+        public async Task<IResult> Handle(CreateProductRequest request, CancellationToken cancellationToken)
+        {
+            if (!request.HttpContextAccessor.HttpContext.User.IsInRole("Administrator") &&
+                !request.AuthService.ValidateToken(request.Token))
+            {
+                return Results.Unauthorized();
+            }
+
+            await request.UnitOfWork.ProductRepository.AddAsync(request.newProduct);
+            await request.UnitOfWork.Save();
+            return Results.Ok("Event deleted");
+        }
+    }
+}
