5bd0b339cd12b84d96ccbdb308068046392084e0
coldmagpie <coldmagpie@gmail.com>
2023-04-19 15:19:04 +0200

    create get reviews from product handler

---
+++ GamesCorner/Server/Handlers/GetReviewsFromProductHandler.cs
@@ -0,0 +1,14 @@
+﻿using GamesCorner.Server.Requests;
+using MediatR;
+
+namespace GamesCorner.Server.Handlers
+{
+    public class GetReviewsFromProductHandler : IRequestHandler<GetReviewsFromProductRequest, IResult>
+    {
+        public async Task<IResult> Handle(GetReviewsFromProductRequest request, CancellationToken cancellationToken)
+        {
+            var reviews = request.UnitOfWork.ProductRepository.GetReviewsFromProduct(request.ProductId);
+            return Results.Ok(reviews);
+        }
+    }
+}
--- GamesCorner/Server/Requests/GetReviewsFromProductRequest.cs
+++ GamesCorner/Server/Requests/GetReviewsFromProductRequest.cs
@@ -3,7 +3,7 @@ using GamesCorner.Server.Requests.Interface;
 
 namespace GamesCorner.Server.Requests
 {
-    public class GetReviewsFromProduct : IHttpRequest
+    public class GetReviewsFromProductRequest : IHttpRequest
     {
          public Guid ProductId { get; set; }
          public HttpContext HttpContext { get; set; }
