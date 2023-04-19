c521b81cdd364a8dc3b963090f9477537214f38c
coldmagpie <coldmagpie@gmail.com>
2023-04-19 12:01:03 +0200

    create cfreate product request

---
+++ GamesCorner/Server/Requests/CreateProductRequest.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess.Models;
+using DataAccess.UnitOfWork;
+using GamesCorner.Server.Requests.Interface;
+
+namespace GamesCorner.Server.Requests
+{
+    public class CreateProductRequest : IHttpRequest
+    {
+        public Guid productId { get; set; }
+        public ProductModel newProduct { get; set; }
+        public IHttpContextAccessor HttpContextAccessor { get; set; }
+        public string Token { get; set; }
+        public IUnitOfWork UnitOfWork { get; set; }
+    }
+}
