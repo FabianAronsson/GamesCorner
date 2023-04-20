ce7113547d47e18825a33f1301a2f8c8b4b9a966
coldmagpie <coldmagpie@gmail.com>
2023-04-20 11:42:44 +0200

    test show reviews in page

--- GamesCorner/Client/Pages/ProductInfo.razor.cs
+++ GamesCorner/Client/Pages/ProductInfo.razor.cs
@@ -1,13 +1,8 @@
 ﻿using System.Net.Http.Json;
-using System.Runtime.CompilerServices;
-using System.Security.Claims;
-using Blazored.LocalStorage;
-using GamesCorner.Client.Services.CartService;
+using System.Text;
 using GamesCorner.Shared.Dtos;
 using GamesCorner.Shared.DTOs;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
-
 namespace GamesCorner.Client.Pages
 {
     partial class ProductInfo : ComponentBase
@@ -15,26 +10,44 @@ namespace GamesCorner.Client.Pages
         [Parameter] public string ProductId { get; set; }
 
         public ProductModelDto Product { get; set; } = new();
-
-        public List<string> _fakeReviews { get; set; } = new() { "This game is an absolute blast to play! The graphics are stunning, the gameplay is addictive, and the storyline is compelling. I love the way the game encourages exploration and experimentation, and the level design is top-notch. Overall, this is one of the best games I've played in years.", "While this game has some interesting ideas and mechanics, it ultimately falls short of its potential. The controls can be frustrating at times, and the pacing feels uneven. The story is also a bit convoluted, and I found it hard to stay engaged. Overall, this game is worth checking out if you're a fan of the genre, but it's not a must-play", "This game is a masterpiece! The world-building is incredible, the characters are well-developed, and the voice acting is top-notch. The gameplay is challenging but rewarding, and there are so many different paths to take that you'll never get bored. If you're a fan of immersive, story-driven games, you absolutely need to check this one out", "I was really excited to play this game, but unfortunately, it didn't live up to my expectations. The graphics are lackluster, and the controls feel clunky and unresponsive. The storyline is also underwhelming, and I found myself struggling to stay interested. Overall, I wouldn't recommend this game unless you're a die-hard fan of the genre.\"", "\"This game is an absolute gem! The art style is gorgeous, the music is beautiful, and the gameplay is innovative and addictive. I love the way the game challenges you to think outside the box and come up with creative solutions to puzzles. Overall, this is one of the best indie games I've played in a long time, and I highly recommend it" };
-
-        public AuthenticationStateProvider _authenticationStateProvider { get; set; }
+        private int i = 0;
+        public List<ReviewsDto>? Reviews { get; set; } = new();
         private string _authMessage;
-
-        private IEnumerable<Claim> _claims = Enumerable.Empty<Claim>();
 
         public string cartText { get; set; } = "Add to cart";
 
+        protected override async Task OnInitializedAsync()
+        {
+            
+        }
+        public static string GetStarRating(double rating)
+        {
+            string fullStar = "★";
+            string emptyStar = "☆";
+            int numberOfFullStars = (int)Math.Floor(rating);
+            bool hasHalfStar = (rating - numberOfFullStars) >= 0.5;
 
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < numberOfFullStars; i++)
+            {
+                sb.Append(fullStar);
+            }
+            if (hasHalfStar)
+            {
+                sb.Append(emptyStar);
+                numberOfFullStars++;
+            }
+            for (int i = numberOfFullStars; i < 5; i++)
+            {
+                sb.Append(emptyStar);
+            }
 
-
+            return sb.ToString();
+        }
         protected override async Task OnParametersSetAsync()
         {
             await GetProduct();
         }
-
-
-
 
         private async Task GetProduct()
         {
@@ -43,15 +56,13 @@ namespace GamesCorner.Client.Pages
 
             var response = await client.GetAsync($"getProduct?id={GuidFromString}");
 
-
-
-
-
             if (response.IsSuccessStatusCode)
             {
                 Product = await response.Content.ReadFromJsonAsync<ProductModelDto>();
-
             }
+
+            var reviewResponse = await client.GetAsync($"productReviews/{ProductId}");
+            Reviews = await reviewResponse.Content.ReadFromJsonAsync<List<ReviewsDto>>();
             StateHasChanged();
         }
         private async Task AddProductToCart()
---
+++ GamesCorner/Shared/Dtos/ReviewsDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesCorner.Shared.Dtos
+{
+    public class ReviewsDto : IEnumerable
+    {
+        public string Id { get; set; }
+        public string Content { get; set; }
+        public string UserEmail { get; set; }
+        public string ProductId { get; set; }
+        public double Rating { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
