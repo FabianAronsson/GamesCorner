692d99b7a0c68f7bc0c636d13c2e80d207a76c88
coldmagpie <coldmagpie@gmail.com>
2023-04-24 10:27:39 +0200

    pushhhhh

---
+++ DataAccess/Migrations/20230424080927_update appcontext.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateappcontext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1b844409-6260-47a8-896d-c94b8867591e");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8aff94d2-9a56-4007-8e6d-50de00bce288");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "06be3b31-05a0-4f2f-8cdd-1a62b075e160", null, "Administrator", "ADMINISTRATOR" },
+                    { "d85b858a-6d53-4b1e-bc6c-4969060dd4bc", null, "Customer", "CUSTOMER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "06be3b31-05a0-4f2f-8cdd-1a62b075e160");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d85b858a-6d53-4b1e-bc6c-4969060dd4bc");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1b844409-6260-47a8-896d-c94b8867591e", null, "Customer", "CUSTOMER" },
+                    { "8aff94d2-9a56-4007-8e6d-50de00bce288", null, "Administrator", "ADMINISTRATOR" }
+                });
+        }
+    }
+}
--- GamesCorner/Server/Handlers/AddToCartHandler.cs
+++ GamesCorner/Server/Handlers/AddToCartHandler.cs
@@ -30,27 +30,34 @@ namespace GamesCorner.Server.Handlers
             var order = orders
                 .FirstOrDefault(o => o.IsActive && o.CustomerEmail.Equals(email));
 
+            var product = await _unitOfWork.ProductRepository.GetAsync(request.item.ProductId);
+
             if (order is null)
             {
+               
                await _unitOfWork.OrderRepository.AddAsync(new OrderModel()
                 {
                     CustomerEmail = (await _userRepository.GetAsync(Guid.Parse(userId))).Email,
                     Id = Guid.NewGuid(),
                     IsActive = true,
-                    Products = new List<OrderItem>(){request.item},
+                    Products = new List<OrderItem>(){new OrderItem()
+                    {
+                        Id = Guid.NewGuid(),
+                        Amount = 1,
+                        ProductId = product.Id}},
                     PurchaseDate = DateTime.UtcNow
                 });
             }
             else
             {
-                var existing = order.Products.FirstOrDefault(o => o.ProductId.Equals(request.item.ProductId));
+                var existing = order.Products.FirstOrDefault(o => o.ProductId.Equals(product.Id));
                 if (existing is not null)
                 {
                     existing.Amount++;
                 }
                 else
                 {
-                    var product = await _unitOfWork.ProductRepository.GetAsync(request.item.ProductId);
+                    //var product = await _unitOfWork.ProductRepository.GetAsync(request.item.ProductId);
                     if (product != null)
                     {
                         var newProduct = new OrderItem()
