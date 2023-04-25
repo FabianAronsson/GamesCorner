594b1b8c87261c80a92a17f3e3ac288382370506
alexgoold <102036021+alexgoold@users.noreply.github.com>
2023-04-25 14:55:47 +0200

    Remove commets

--- GamesCorner/Client/Services/MessageService/IMessageService.cs
+++ GamesCorner/Client/Services/MessageService/IMessageService.cs
@@ -2,11 +2,7 @@
 {
 	public interface IMessageService
 	{
-		//create message service that will send an int from the shopping cart to the navbar to update the cart amount
-
 		event Action<int> OnChange;
 		void UpdateCartAmount(int amount);
-
-
 	}
 }
